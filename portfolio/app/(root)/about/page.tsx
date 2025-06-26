f1651c9d2d1c3da692a44fc2d7db85e333db6dc3
thereisnowithout <129658842+thereisnowithout@users.noreply.github.com>
2025-06-26 19:00:00 +0000

    Implemented home page and about page ready for content

---
+++ portfolio/app/(root)/about/page.tsx
@@ -0,0 +1,9 @@
+// pages/about.tsx
+export default function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>I am a dedicated developer building web applications with Next.Js</p>
+        </div>
+    )
+}
