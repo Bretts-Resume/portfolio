e5b2fe5ce2f52e5032ddf11c8f6809de82b2835e
thereisnowithout <129658842+thereisnowithout@users.noreply.github.com>
2025-07-15 12:00:39 +0000

    Altered page for coming soon

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,15 +4,13 @@ export default function Home() {
   return (
     <div>
       <img src="/IMG_0818.png" alt="" className="w-1/3 h-auto ms-3 personal-image" />
-      <div className="border p-3 text-me max-w-screen me-150 pb-100">
-        <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">Hi, I’m Brett - a web developer focused on building modern, responsive websites with Next.js and Tailwind CSS.  I craft fast, responsive and user-first websites using modern frameworks like Next.js and express.js.
+      <div className="border p-3 text-me max-w-screen pt-50 ps-20 me-150 pb-50">
+        <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">Game design in motion - solo projects and experiments by Brett Grentell.
         </p><br></br>
-        <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">I build custom websites and apps with code that’s as clean as my workspace and UI/UX design to bring ideas to life.
+        <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">Currently studying game design & development.  Portfolio coming soon.
         </p><br></br>
         <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">Let’s build something great together.
         </p>
-        <br></br>
-        <p className="max-w-screen text-sm md:text-base lg:text-lg pe-10">[View my work] or [Get in touch]</p>
       </div>
     </div>
   );
